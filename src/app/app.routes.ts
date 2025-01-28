4ded664323c2650afa4db95b93b9a0d22ad4e6ba
Alison Pinheiro <j.alison955@gmail.com>
2025-01-27 21:45:36 -0400

    componente categorias criado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CategoryComponent } from './pages/category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'category', component: CategoryComponent },
+];
